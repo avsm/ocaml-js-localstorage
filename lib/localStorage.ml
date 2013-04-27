e6239e19dfb79b6e8436dad326775bc694f32367
Anil Madhavapeddy <anil@recoil.org>
2013-04-27 12:19:40 +0100

    add LocalStorage module

---
+++ lib/localStorage.ml
@@ -0,0 +1,63 @@
+(*
+ * Copyright (c) 2010 Anil Madhavapeddy <anil@recoil.org>
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ *)
+
+open Lwt
+open Js
+
+let error f = Printf.ksprintf (fun s -> Firebug.console##error (Js.string s); failwith s) f
+let debug f = Printf.ksprintf (fun s -> Firebug.console##log(Js.string s)) f
+
+type jsstring = Js.js_string Js.t
+
+class type js_store =
+  object
+     method get : jsstring -> jsstring option
+     method set : jsstring -> jsstring -> unit
+  end
+
+let mem_store () : js_store =
+  debug "Using memory storage";
+  let h = Hashtbl.create 1 in
+  object
+    method get k =
+      if Hashtbl.mem h k then
+        Some (Hashtbl.find h k)
+      else
+        None
+
+    method set k v =
+      Hashtbl.replace h k v
+  end
+
+let html5_store ls : js_store =
+  debug "Using HTML5 local storage";
+  object
+    method get k =
+      Js.Opt.to_option ls##getItem(k)
+
+    method set k v  =
+      ls##setItem(k,v)
+  end
+  
+let init () =
+  try
+    Js.Optdef.case 
+      (Dom_html.window##localStorage) 
+      (mem_store)
+      (fun ls -> html5_store ls)
+  with exn ->
+    (* This can be a DOM SecurityError *)
+    mem_store ()
---
+++ lib_test/looper.ml
@@ -0,0 +1,47 @@
+(*
+ * Copyright (c) 2012 Anil Madhavapeddy <anil@recoil.org>
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ *)
+
+open Lwt
+open Js
+
+let error f = Printf.ksprintf (fun s -> Firebug.console##error (Js.string s); failwith s) f
+let debug f = Printf.ksprintf (fun s -> Firebug.console##log(Js.string s)) f
+
+let localStorage () =
+  let st = LocalStorage.init () in
+  let key = Js.string "testKey" in
+  let rec looper n () =
+    debug "loop %d" n;
+    st#set key (Js.string (string_of_int n));
+    Lwt_js.sleep 1.0
+    >>= looper (succ n)
+  in
+  let startVal =
+     match st#get key with
+     |None -> debug "initialising key to 0"; 0
+     |Some k -> 
+      let v = int_of_string (Js.to_string k) in
+      debug "Found start key %d" v;
+      v
+  in
+  looper startVal ()
+
+let go _ = ignore (
+  catch (fun () -> localStorage ())
+    (fun exn -> error "uncaught exception: %s" (Printexc.to_string exn)));
+  _true
+
+let _ = Dom_html.window##onload <- Dom_html.handler go
